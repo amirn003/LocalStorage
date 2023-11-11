afdbf01880fc2ca614ca4c604fc7e89dc6394335
amirn003 <amir.naar@gmail.com>
2023-11-11 11:20:08 +0100

    add items to localStorage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -12,6 +12,7 @@ function addItem(e) {
   console.log(item);
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
   this.reset();
 }
 
@@ -29,3 +30,4 @@ function populateList(plates = [], platesList) {
 }
 
 addItems.addEventListener('submit', addItem);
+populateList(items, itemsList);
